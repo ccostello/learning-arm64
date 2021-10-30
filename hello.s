d038c07e9c27a34155f8e2cb72820f10cfc5490a
Chris Costello <chris@costello.nyc>
2021-10-30 17:37:32 -0400

    Greet the world only 10 times

--- hello.s
+++ hello.s
@@ -1,17 +1,22 @@
 .global _start
 .align 2
 
-_start: mov X0, #1
-    adr X1, helloworld
-    mov X2, #13
-    mov X16, #4
-    svc 0
+_start: 
+    ; TODO: is X5 safe?
+    mov X5, #10         ; counter init
+again:
+    mov X0, #1          ; stdout
+    adr X1, helloworld  ; string value
+    mov X2, #13         ; string size
+    mov X16, #4         ; syscall to write
+    svc 0               ; invoke syscall
 
-    // Loop forever
-    b _start
+    add X5,X5,#-1       ; decrement counter
+    cmp X5,0            ; are we there yet?
+    bge again           ; if not, try again
 
-    mov X0, #0
-    mov X16, #1
-    svc 0
+    mov X0, #0          ; exit code
+    mov X16, #1         ; exit syscall
+    svc 0               ; invoke syscall
 
 helloworld: .ascii "Hello World!\n"
